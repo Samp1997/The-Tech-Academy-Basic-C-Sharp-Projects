0d676e56018f5c3b62bdff158890b29500a02e07
Samp1997 <130931137+Samp1997@users.noreply.github.com>
2023-11-05 20:05:30 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            //this is a simple program
+            Console.WriteLine("the tech academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("Enter your name here: ");
+            string yourname = Console.ReadLine();
+            Console.WriteLine("hello, " + yourname + "!");
+            Console.WriteLine("You are currenly on the C# course");
+            Console.ReadLine();
+            Console.WriteLine("On page 163");
+            Console.ReadLine();
+            Console.WriteLine("Do you need help with anything?: ");
+            string TorF = Console.ReadLine();
+            Console.WriteLine( TorF  + " ,if true an instructor will reach out soon!" );
+            Console.ReadLine();
+            Console.WriteLine("Was ther any positive experience you'd like to share?: ");
+            string question3Awnser = Console.ReadLine();
+            Console.WriteLine(question3Awnser + ",  Thank you for the feedback!");
+            Console.ReadLine();
+            Console.WriteLine("Is there any feedback youd like to provide? Please be specific: ");
+            string question4Awnser = Console.ReadLine();
+            Console.WriteLine(question4Awnser + " Thank you for the feedback!");
+            Console.ReadLine();
+            Console.WriteLine("How many hours did you study?: ");
+            string question5Awnser = Console.ReadLine();
+            Console.WriteLine("Good Job on wokring " + question5Awnser + " hours today!");
+            Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!: ");
+            Console.ReadLine();
+
+
+
+
+
+
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            //bool isStudying = false;
+            //byte hoursWorked = 42;
+            //sbyte currentTemperture = -23;
+            //char questionMark = '\u2103';
+
+            //decimal moneyIntheBank = 100.5m;
+
+            //double heightincentimeters = 4354.8456;
+
+            //float secondsLeft = 2.62f;
+
+            //int currentAge = 30;
+            //string yearsOld() = currentAge.ToString();
+
+            //Console.WriteLine(questionMark);
+            //Console.ReadLine();
+
+
+        }
+    }
+}

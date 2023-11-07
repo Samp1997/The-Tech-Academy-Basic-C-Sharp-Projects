19fc3be73ab52f127310490e455a6d207049147c
Samp1997 <130931137+Samp1997@users.noreply.github.com>
2023-11-06 22:04:57 -0800

    Add files via upload

---
+++ MathandComparison/MathandComparison/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MathandComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.ReadLine();
+            Console.WriteLine("Person 1");
+            Console.ReadLine();
+            Console.WriteLine("Hourly Rate?");
+            Console.WriteLine("15");
+            Console.ReadLine();
+            Console.WriteLine("Hours worked per week?");
+            Console.WriteLine("40");
+            Console.ReadLine();
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate?");
+            Console.WriteLine("20");
+            Console.WriteLine("Hours worked per week?");
+            Console.WriteLine("40");
+            Console.ReadLine();
+            Console.WriteLine("Annual salary of Person 1: 31,200");
+            Console.WriteLine("Annual salary of Person 2: 41,600");
+            Console.ReadLine();
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            string TorF = Console.ReadLine();
+            Console.WriteLine(TorF + "  Person 2 makes more money");
+        }
+    }
+}
